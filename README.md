81695c63e0c47fd5924d21beb2496f061c407630
Ellie <lou.luck@proton.me>
2024-06-27 06:32:10 +0200

    added .env setup

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # tfgames-bot
+
+
+## Local Setup
+- replace the api-token in the .env file with an actual token
+- dunno figure it out from there buddy
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
 ﻿import { SapphireClient } from '@sapphire/framework';
-import { GatewayIntentBits } from 'discord.js';
+import { GatewayIntentBits } from 'discord.js'
+// @ts-ignore
+import dotenv from 'dotenv';
 
 const client = new SapphireClient({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages] });
-
+dotenv.config();  // Load environment variables from .env file 
+const apiKey = process.env.DISCORD_API_TOKEN;  // Retrieve the environment variable 
+console.log('API Key:', apiKey)
 client.login('your-token-goes-here');
