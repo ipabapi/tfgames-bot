ba00dddd663ce35bf6a9c8b9e8490665f5705076
Ellie <lou.luck@proton.me>
2024-06-27 06:09:25 +0200

    initial project setup

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+﻿import { SapphireClient } from '@sapphire/framework';
+import { GatewayIntentBits } from 'discord.js';
+
+const client = new SapphireClient({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages] });
+
+client.login('your-token-goes-here');
