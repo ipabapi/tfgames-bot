cf03c5612d61c09330b117f87401527197d747f3
Ellie <lou.luck@proton.me>
2024-06-27 07:06:48 +0200

    added basic command for testing

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,10 @@ import { GatewayIntentBits } from 'discord.js'
 // @ts-ignore
 import dotenv from 'dotenv';
 
-const client = new SapphireClient({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages] });
+const client = new SapphireClient({ 
+    intents: [GatewayIntentBits.MessageContent, GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages],
+    loadMessageCommandListeners: true});
 dotenv.config();  // Load environment variables from .env file 
 const apiKey = process.env.DISCORD_API_TOKEN;  // Retrieve the environment variable 
 console.log('API Key:', apiKey)
-client.login('your-token-goes-here');
+client.login(apiKey);
