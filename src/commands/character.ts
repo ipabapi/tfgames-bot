bb2f88da68e202c81fa399571be990375cf23d06
Zeke DuBois <10969639+ipabapi@users.noreply.github.com>
2024-07-01 12:34:58 -0700

    Character complete, needs refactor

--- src/commands/character.ts
+++ src/commands/character.ts
@@ -1,6 +1,6 @@
 import { Subcommand } from '@sapphire/plugin-subcommands';
 // import { container } from "@sapphire/framework";
-import { ComponentType, Message, MessageCollector } from 'discord.js';
+import { ComponentType, EmbedType, Message, MessageCollector } from 'discord.js';
 import { Character, CharacterMode } from '../lib/bot.types';
 import { initialCharacter } from '../lib/initials';
 import { MessageBuilder } from '@sapphire/discord.js-utilities';
@@ -46,11 +46,6 @@ export class CharacterCommand extends Subcommand {
 					name: 'list',
 					messageRun: 'list',
 					chatInputRun: 'list'
-				},
-				{
-					name: 'help',
-					messageRun: 'help',
-					chatInputRun: 'help'
 				}
 			]
 		});
@@ -111,11 +106,283 @@ export class CharacterCommand extends Subcommand {
 	}
 
 	public async info(interaction: Subcommand.ChatInputCommandInteraction) {
-		await interaction.reply('info');
+		let user;
+		if (interaction.options.getUser('user')) {
+			user = await this.container.mongoClient
+				.db('test')
+				.collection('users')
+				// @ts-ignore
+				.findOne({ userId: interaction.options.getUser('user').id });
+			if (!user) {
+				interaction.reply({ content: "I'm sorry, I couldn't find that user in the database.", ephemeral: true });
+				return;
+			}
+		} else {
+			user = await this.container.mongoClient.db('test').collection('users').findOne({ userId: interaction.user.id });
+			if (!user) {
+				interaction.reply({ content: "I'm sorry, you need to set up your account first. Please use the /setup command.", ephemeral: true });
+				return;
+			}
+		}
+
+		const character = await this.container.mongoClient
+			.db('test')
+			.collection('characters')
+			.findOne({ name: interaction.options.getString('name'), creator: user.userId });
+		if (!character) {
+			interaction.reply({ content: "I'm sorry, I couldn't find a character with that name.", ephemeral: true });
+			return;
+		}
+		const username = interaction.options.getUser('user')?.displayName || interaction.user.username;
+
+		interaction.reply({
+			embeds: [
+				{
+					title: character.name,
+					type: EmbedType.Rich,
+					description: character.description,
+					image: {
+						url: character.avatar
+					},
+					fields: [
+						{
+							name: 'Mode',
+							value: character.mode
+						},
+						{
+							name: 'Creator',
+							value: username
+						},
+						{
+							name: 'Mental Effects',
+							value: character.mentalEffects.join(', ') || 'None'
+						},
+						{
+							name: 'Physical Effects',
+							value: character.physicalEffects.join(', ') || 'None'
+						},
+						{
+							name: 'Mind Broken',
+							value: character.mindBroken ? 'Yes' : 'No'
+						},
+						{
+							name: 'Collared',
+							value: character.collared ? `Yes, by ${character.collarer}` : 'No'
+						}
+					],
+					footer: {
+						text: `Created by ${username}. | Requested by ${interaction.user.username}`,
+						icon_url: interaction.user.displayAvatarURL()
+					}
+				}
+			]
+		});
 	}
 
 	public async edit(interaction: Subcommand.ChatInputCommandInteraction) {
-		await interaction.reply('edit');
+		// Check if the user is in the database
+		let user;
+		if (interaction.options.getUser('user')) {
+			user = await this.container.mongoClient
+				.db('test')
+				.collection('users')
+				// @ts-ignore
+				.findOne({ userId: interaction.options.getUser('user').id });
+			if (!user) {
+				interaction.reply({ content: "I'm sorry, I couldn't find that user in the database.", ephemeral: true });
+				return;
+			}
+		} else {
+			user = await this.container.mongoClient.db('test').collection('users').findOne({ userId: interaction.user.id });
+			if (!user) {
+				interaction.reply({ content: "I'm sorry, you need to set up your account first. Please use the /setup command.", ephemeral: true });
+				return;
+			}
+		}
+
+		// check if the character exists
+		const character = await this.container.mongoClient
+			.db('test')
+			.collection('characters')
+			.findOne({ name: interaction.options.getString('name'), creator: user.userId });
+		if (!character) {
+			interaction.reply({ content: "I'm sorry, I couldn't find a character with that name.", ephemeral: true });
+			return;
+		}
+        if (character.collared && character.collarer !== interaction.user.id) {
+            interaction.reply({ content: "I'm sorry, you can't edit a collared character that isn't yours.", ephemeral: true });
+            return;
+        }
+        if (!character.collared && character.creator !== interaction.user.id) {
+            interaction.reply({ content: "I'm sorry, you can't edit a character that isn't yours.", ephemeral: true });
+            return;
+        }
+
+		let successful = false;
+		let newChara = character;
+		interaction.user
+			.send("Hello! Let's edit your character! What would you like to edit? Please provide the new value.")
+			.then(async (msg) => {
+				let currentStep = 0;
+				const collector = new MessageCollector(msg.channel, { filter: (m) => m.author.id == interaction.user.id, time: 60000 });
+				collector.on('collect', async (message: Message) => {
+					if (message.content.toLowerCase() === 'cancel') {
+						message.channel.send('Character editing cancelled.');
+						collector.stop();
+						return;
+					}
+					switch (currentStep) {
+						case 0:
+							// Name
+							newChara.name = message.content;
+							// Check if the name is already taken for this user
+							const checkName = await this.container.mongoClient
+								.db('test')
+								.collection('characters')
+								.findOne({ name: message.content, creator: user.userId });
+							if (checkName) {
+								message.channel.send("I'm sorry, you already have a character with that name. Please choose a different name.");
+								return;
+							}
+							currentStep++;
+							// console.log(name, currentStep, msg.author.id);
+							message.channel.send(
+								`Great! Your character's name is now ${message.content}. What does your character look like? Please provide an image or URL. If you don't have one, simply type "none".`
+							);
+							break;
+						case 1:
+							// Avatar
+							if (message.content.toLowerCase() === 'none') {
+								currentStep++;
+								message.channel.send("That's okay! We can always add one later. How would you describe your character?");
+							} else {
+								// Check if user provided an attachment
+								if (message.attachments.size > 0) {
+									// @ts-ignore
+									newChara.avatar = message.attachments.first().url;
+								} else {
+									// Check if user provided a URL
+									if (message.content.startsWith('http')) {
+										newChara.avatar = message.content;
+									} else {
+										message.channel.send('I need an image or URL. Please try again.');
+										return;
+									}
+								}
+								currentStep++;
+								message.channel.send('Great! How would you describe your character?');
+							}
+							break;
+						case 2:
+							// Description
+							newChara.description = message.content;
+							currentStep++;
+							message.channel
+								.send(
+									new MessageBuilder()
+										.setEmbeds([
+											{
+												title: 'Difficulty Selection',
+												description:
+													'Okay, last step! Please select the difficulty you would like to play on.\n\n- **Normal**: Base mode, character can gain and store gold, as well as use items.\n- **Hard**: Character cannot gain or store gold, but can use items.\n- **Harder**: Character *CAN* gain and store gold, but cannot use items.\n- **Very Hard**: Character cannot gain or store gold, and cannot use items.\n- **Nightmare**: Character cannot gain or store gold, cannot use items, and will rely on the bot to draw for them, as well as decide if they pass, reroll, or play.\n\nPlease select a difficulty.',
+												color: 0x00ff00
+											}
+										])
+										.setComponents([
+											{
+												type: 1,
+												components: [
+													{
+														type: 3,
+														customId: 'difficulty',
+														options: [
+															{
+																label: 'Normal',
+																value: 'normal'
+															},
+															{
+																label: 'Hard',
+																value: 'hard'
+															},
+															{
+																label: 'Harder',
+																value: 'harder'
+															},
+															{
+																label: 'Very Hard',
+																value: 'veryhard'
+															},
+															{
+																label: 'Nightmare',
+																value: 'nightmare'
+															}
+														]
+													}
+												]
+											}
+										])
+								)
+								.then((m) => {
+									let success = false;
+									const collector = m.createMessageComponentCollector({ componentType: ComponentType.StringSelect, time: 60000 });
+									collector.on('collect', async (i) => {
+										//@ts-ignore
+										newChara.mode = difficulties[i.values[0]];
+										newChara.creator = interaction.user.id;
+										success = true;
+										try {
+											const result = await this.container.mongoClient
+												.db('test')
+												.collection('characters')
+												.updateOne({ name: interaction.options.getString('name'), creator: user.userId }, { $set: newChara });
+											if (result.modifiedCount === 1) {
+												message.channel.send('Character editing complete! You can now use this character in games.');
+											} else {
+												message.channel.send('Character editing failed. Please try again.');
+											}
+										} catch (err) {
+											console.error(err);
+											message.channel.send(
+												'Character editing failed! It was on our end, not yours. Please try again in a bit.'
+											);
+										}
+										m.delete();
+
+										collector.stop();
+									});
+									collector.on('end', () => {
+										if (!success) {
+											message.channel.send('Character editing timed out. Please try again.');
+											collector.stop();
+										}
+									});
+								});
+							break;
+
+						default:
+							message.channel.send("I'm sorry, I didn't understand that. Please try again.");
+							console.error('Invalid step reached in character editing.');
+							successful = false;
+							collector.stop();
+							break;
+					}
+					if (currentStep > 2) {
+						successful = true;
+						collector.stop();
+					}
+				});
+				collector.on('end', (collected) => {
+					if (!successful) {
+						msg.channel.send('Character editing timed out. Please try again.');
+					}
+					console.log(`Collected ${collected.size} messages. Successful: ${successful}.`);
+				});
+			})
+			.catch((err) => {
+				console.error(err);
+				interaction.reply("I'm sorry, I couldn't send you a DM. Please make sure your DMs are open and try again.");
+			});
+		await interaction.reply({ content: "I've sent you a DM to start character editing!", ephemeral: true });
 	}
 
 	public async create(interaction: Subcommand.ChatInputCommandInteraction) {
@@ -347,7 +614,7 @@ export class CharacterCommand extends Subcommand {
 			user = await this.container.mongoClient
 				.db('test')
 				.collection('users')
-                // @ts-ignore
+				// @ts-ignore
 				.findOne({ userId: interaction.options.getUser('user').id });
 			if (!user) {
 				interaction.reply({ content: "I'm sorry, I couldn't find that user in the database.", ephemeral: true });
@@ -363,7 +630,10 @@ export class CharacterCommand extends Subcommand {
 
 		const characters = await this.container.mongoClient.db('test').collection('characters').find({ creator: user.userId }).toArray();
 		if (characters.length === 0) {
-			interaction.reply({ content: `${user.userId == interaction.user.id ? "You don't" : "This user doesn't"} have any characters yet! Use /character create to make one.`, ephemeral: true });
+			interaction.reply({
+				content: `${user.userId == interaction.user.id ? "You don't" : "This user doesn't"} have any characters yet! Use /character create to make one.`,
+				ephemeral: true
+			});
 			return;
 		}
 
@@ -374,8 +644,8 @@ export class CharacterCommand extends Subcommand {
 				value: c.description
 			};
 		});
-        // get user name if it's not the author
-        const username = interaction.options.getUser('user')?.displayName || interaction.user.username;
+		// get user name if it's not the author
+		const username = interaction.options.getUser('user')?.displayName || interaction.user.username;
 		interaction.reply({
 			embeds: [
 				{
